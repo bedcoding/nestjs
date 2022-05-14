8173c1dabb7ab986f73ac9fe40fd5fbbdbe13cd0
ggavi2000 <ggavi2000@naver.com>
2022-05-15 01:29:31 +0900

    ArgsType 검증 (예: 5자 이상 10자 이하만 받는 식)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe)  // dto에 넣은 제약조건 @IsString() @Length(5, 10) 같은거 발동시키기
   await app.listen(3000);
 }
 
--- src/sample/dtos/create-sample-dto.ts
+++ src/sample/dtos/create-sample-dto.ts
@@ -1,17 +1,32 @@
 import { ArgsType, Field } from "@nestjs/graphql";
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateSampleDto {
     @Field(type => String)
+    @IsString()
+    @Length(5, 10)  // 5~10자 제한
     name: string;
 
     @Field(type => Boolean)
+    @IsBoolean()
     isVegan: boolean;
 
     @Field(type => String)
+    @IsString()
     address: string;
 
     @Field(type => String)
     ownersName: string;
-
 }
+
+/*
+mutation {
+  createSample(
+  name: "12345"
+  isVegan: true
+  address: "1"
+  ownersName: "1"
+  )
+}
+*/
