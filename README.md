fe5326d573ba7a338d2b895235d231da55eee891
ggavi2000 <ggavi2000@naver.com>
2022-05-15 14:57:48 +0900

    typeorm을 이용한 db연동

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 3. nest g mo sample  (모듈 생성)
 4. 공식문서(https://docs.nestjs.com/graphql/quick-start#getting-started-with-graphql--typescript)에 따르면 graphql 연동 방식이 수정되서 추가 설치가 필요함 (npm i @nestjs/graphql @nestjs/apollo graphql apollo-server-express)
 5. 연동 후 localhost:3000/graphql
-6. 
+6. DB연동을 위해 타입ORM 추가 (npm install --save @nestjs/typeorm typeorm pg)
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,28 @@
 import { Module } from '@nestjs/common';
 import { ApolloDriver } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SampleModule } from './sample/sample.module';
 import { join } from 'path';
 
 @Module({
-  imports: [GraphQLModule.forRoot({
+  imports: [
+    SampleModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '',
+      port: 5432,
+      username: 'study',
+      password: '',
+      database: 'study',
+      synchronize: false,  // 소스코드 컬럼에 변화가 생길 때 데이터베이스도 바꿔버림
+      logging: true,  // 로그
+    }),
+    GraphQLModule.forRoot({
       // autoSchemaFile: join(process.cwd(), 'src/schema.gql'),  // 자동 Schema 파일 생성
       autoSchemaFile: true,  // 위에서처럼 별도 파일을 만들지 말고 그냥 메모리에 저장해두는 경우
       driver: ApolloDriver,
     }), 
-    
-    SampleModule
   ],
   controllers: [],
   providers: [],
